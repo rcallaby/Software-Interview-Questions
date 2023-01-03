49810334908923e793b5e89bf0b12e7e4bf6d8ba
Richard Callaby <rcallaby@gmail.com>
2023-01-03 07:30:07 -0600

    added some solutions

---
+++ Miscellaneous Questions/Algorithms in Python/bubbleSort.py
@@ -0,0 +1,18 @@
+# This is a demonstration of the bubblesort algorithm in Python without using any libraries.
+
+def bubblesort (arr):
+	# loop through entire array
+	for i in range(len(arr)):
+		# loop through array up to the i-th element
+		for j in range(len(arr)-1):
+			# compare elements
+			if arr[j] > arr[j+1]:
+				# swap elements
+				temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+	return arr
+
+# testing
+my_array = [3,2,1,5,4]
+print(bubblesort(my_array))
