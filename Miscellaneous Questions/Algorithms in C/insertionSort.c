15b4614024b7efe276a2083888a2086af0f6d145
Richard Callaby <rcallaby@gmail.com>
2023-01-04 09:15:52 -0600

    added some more solutions

---
+++ Miscellaneous Questions/Algorithms in C/insertionSort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+    This is a possible solution for a insertion sort algorithm as you would find in a technical interview
+    I predict I will probably refactor it in the future.
+*/
+ 
+int main()
+{
+  int n, array[1000], c, d, t;
+ 
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+ 
+  printf("Enter %d integers\n", n);
+ 
+  for (c = 0; c < n; c++) {
+    scanf("%d", &array[c]);
+  }
+ 
+  for (c = 1 ; c <= n - 1; c++) {
+    d = c;
+ 
+    while ( d > 0 && array[d] < array[d-1]) {
+      t          = array[d];
+      array[d]   = array[d-1];
+      array[d-1] = t;
+ 
+      d--;
+    }
+  }
+ 
+  printf("Sorted list in ascending order:\n");
+ 
+  for (c = 0; c <= n - 1; c++) {
+    printf("%d\n", array[c]);
+  }
+ 
+  return 0;
