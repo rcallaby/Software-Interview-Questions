def7bcec377ddf16b06cece0b00308ad8ce1b794
Richard Callaby <rcallaby@gmail.com>
2023-02-12 16:57:54 -0600

    corrected errors

--- Image Processing/Seam Carving/seamCarving.c
+++ Image Processing/Seam Carving/seamCarving.c
@@ -4,37 +4,57 @@
 #define M 400
 #define N 600
 
-//TODO: Fix syntax and other computational errors
 //TODO: Add a function to find the lowest energy seam and remove it
 
 int image[M][N];
 int energy[M][N];
 
-int min(int a, int b, int c) {
+int minimum(int a, int b, int c)
+{
     int min = a;
-    if (b < min) min = b;
-    if (c < min) min = c;
+    if (b < min)
+        min = b;
+    if (c < min)
+        min = c;
     return min;
 }
 
-void compute_energy(void) {
+void compute_energy(void)
+{
     int i, j;
-    for (i = 0; i < M; i++) {
-        for (j = 0; j < N; j++) {
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
             int left, right, top, bottom;
-            if (j == 0) left = 0x3f3f3f3f; else left = abs(image[i][j-1] - image[i][j]);
-            if (j == N-1) right = 0x3f3f3f3f; else right = abs(image[i][j+1] - image[i][j]);
-            if (i == 0) top = 0x3f3f3f3f; else top = abs(image[i-1][j] - image[i][j]);
-            if (i == M-1) bottom = 0x3f3f3f3f; else bottom = abs(image[i+1][j] - image[i][j]);
-            energy[i][j] = min(left, right, min(top, bottom));
+            if (j == 0)
+                left = 0x3f3f3f3f;
+            else
+                left = abs(image[i][j - 1] - image[i][j]);
+            if (j == N - 1)
+                right = 0x3f3f3f3f;
+            else
+                right = abs(image[i][j + 1] - image[i][j]);
+            if (i == 0)
+                top = 0x3f3f3f3f;
+            else
+                top = abs(image[i - 1][j] - image[i][j]);
+            if (i == M - 1)
+                bottom = 0x3f3f3f3f;
+            else
+                bottom = abs(image[i + 1][j] - image[i][j]);
+            energy[i][j] = mininum(left, right, (minimum(top, bottom)));
         }
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int i, j;
-    for (i = 0; i < M; i++) {
-        for (j = 0; j < N; j++) {
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
             // Read in image data
             scanf("%d", &image[i][j]);
         }
--- Linked List Algorithms/Algorithms in C/skipList.c
+++ Linked List Algorithms/Algorithms in C/skipList.c
@@ -41,104 +41,134 @@ int random_level() {
     return level;
 }
 
-void insert(struct skip_list *list, int key, int value) {
+void insert(struct skip_list *list, int key, int value)
+{
     struct node *update[MAX_LEVEL];
     struct node *node = list->head;
-    for (int i = list->level; i >= 0; i--) {
-        while (node->forward[i] != NULL && node->forward[i]->key < key) {
+    for (int i = list->level; i >= 0; i--)
+    {
+        while (node->forward[i] != NULL && node->forward[i]->key < key)
+        {
             node = node->forward[i];
         }
         update[i] = node;
     }
     node = node->forward[0];
-    if (node == NULL || node->key != key) {
+    if (node == NULL || node->key != key)
+    {
         int level = random_level();
-        if (level > list->level) {
-            for (int i = list->level + 1; i < level; i++) {
+        if (level > list->level)
+        {
+            for (int i = list->level + 1; i < level; i++)
+            {
                 update[i] = list->head;
             }
             list->level = level;
         }
         node = new_node(level, key, value);
-        for (int i = 0; i < level; i++) {
+        for (int i = 0; i < level; i++)
+        {
             node->forward[i] = update[i]->forward[i];
             update[i]->forward[i] = node;
         }
-    } else {
+    }
+    else
+    {
         node->value = value;
     }
 }
 
-void delete(struct skip_list *list, int key) {
+void delete(struct skip_list *list, int key)
+{
     struct node *update[MAX_LEVEL];
     struct node *node = list->head;
-    for (int i = list->level; i >= 0; i--) {
-        while (node->forward[i] != NULL && node->forward[i]->key < key) {
+    for (int i = list->level; i >= 0; i--)
+    {
+        while (node->forward[i] != NULL && node->forward[i]->key < key)
+        {
             node = node->forward[i];
         }
         update[i] = node;
     }
     node = node->forward[0];
-    if (node != NULL && node->key == key) {
-            for (int i = 0; i <= list->level; i++) {
-        if (update[i]->forward[i] != node) {
-            break;
+    if (node != NULL && node->key == key)
+    {
+        for (int i = 0; i <= list->level; i++)
+        {
+            if (update[i]->forward[i] != node)
+            {
+                break;
+            }
+            update[i]->forward[i] = node->forward[i];
         }
-        update[i]->forward[i] = node->forward[i];
-    }
-    free(node->forward);
-    free(node);
-    while (list->level > 0 && list->head->forward[list->level] == NULL) {
-        (list->level--);
-    }
-}
-
-struct node *search(struct skip_list *list, int key) {
-    struct node *node = list->head;
-    for (int i = list->level; i >= 0; i--) {
-        while (node->forward[i] != NULL && node->forward[i]->key < key) {
-            node = node->forward[i];
+        free(node->forward);
+        free(node);
+        while (list->level > 0 && list->head->forward[list->level] == NULL)
+        {
+            (list->level--);
         }
     }
-    node = node->forward[0];
-    if (node != NULL && node->key == key) {
-        return node;
-    } else {
-        return NULL;
-    }
 }
 
-void print_list(struct skip_list *list) {
-    for (int i = list->level; i >= 0; i--) {
-        printf("Level %d: ", i);
-        struct node *node = list->head->forward[i];
-        while (node != NULL) {
-            printf("(%d, %d) ", node->key, node->value);
-            node = node->forward[i];
+    struct node *search(struct skip_list *list, int key)
+    {
+        struct node *node = list->head;
+        for (int i = list->level; i >= 0; i--)
+        {
+            while (node->forward[i] != NULL && node->forward[i]->key < key)
+            {
+                node = node->forward[i];
+            }
         }
-        printf("\n");
+        node = node->forward[0];
+        if (node != NULL && node->key == key)
+        {
+            return node;
+        }
+        else
+        {
+            return NULL;
+        }
     }
-}
 
-int main() {
-    srand(time(NULL));
-    struct skip_list *list = create_skip_list();
-    insert(list, 1, 10);
-    insert(list, 2, 20);
-    insert(list, 3, 30);
-    insert(list, 4, 40);
-    insert(list, 5, 50);
-    printf("Skip list after inserting (1, 10), (2, 20), (3, 30), (4, 40), (5, 50):\n");
-    print_list(list);
-    delete(list, 4);
-    printf("Skip list after deleting (4, 40):\n");
-    print_list(list);
-    struct node *result = search(list, 3);
-    if (result != NULL) {
-        printf("Found key 3 with value %d\n", result->value);
-    } else {
-        printf("Key 3 not found\n");
+
+    void print_list(struct skip_list * list)
+    {
+        for (int i = list->level; i >= 0; i--)
+        {
+            printf("Level %d: ", i);
+            struct node *node = list->head->forward[i];
+            while (node != NULL)
+            {
+                printf("(%d, %d) ", node->key, node->value);
+                node = node->forward[i];
+            }
+            printf("\n");
+        }
     }
-    return 0;
-}
 
+    int main()
+    {
+        srand(time(NULL));
+        struct skip_list *list = create_skip_list();
+        insert(list, 1, 10);
+        insert(list, 2, 20);
+        insert(list, 3, 30);
+        insert(list, 4, 40);
+        insert(list, 5, 50);
+        printf("Skip list after inserting (1, 10), (2, 20), (3, 30), (4, 40), (5, 50):\n");
+        print_list(list);
+        delete (list, 4);
+        printf("Skip list after deleting (4, 40):\n");
+        print_list(list);
+        struct node *result = search(list, 3);
+        if (result != NULL)
+        {
+            printf("Found key 3 with value %d\n", result->value);
+        }
+        else
+        {
+            printf("Key 3 not found\n");
+        }
+        return 0;
+    }
