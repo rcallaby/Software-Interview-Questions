9c75c53af8be30e35decc641176d9b22f48c09b6
rcallaby <rcallaby@gmail.com>
2024-02-27 19:13:16 -0600

    refactored the leap year example

--- Miscellaneous-Questions/Algorithms in C/isLeapYear.c
+++ Miscellaneous-Questions/Algorithms in C/isLeapYear.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
-int main()
-{
-	int year;
+int IsLeapYear(int year);
+
+int main() {
+    int year;
 	
-	printf("Please enter the year: ");
-	scanf("%d", &year);
-	
-	if (year % 4 == 0)
-	{
-		if (year % 100 == 0)
-		{
-			if (year % 400 == 0)
-				printf("%d is a leap year\n", year);
-			else
-				printf("%d is not a leap year\n", year);
-		}
-		else
-			printf("%d is a leap year\n", year);
-	}
-	else
-		printf("%d is not a leap year\n", year);
-	
-	return 0;
+    printf("Please enter the year: ");
+    scanf("%d", &year);
+
+    if (IsLeapYear(year)) {
+        printf("%d is a leap year\n", year);
+    } else {
+        printf("%d is not a leap year\n", year);
+    }
+
+    return 0;
+}
+
+int IsLeapYear(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                return 1;
+            else
+                return 0;
+        } else {
+            return 1;
+        }
+    } else {
+        return 0;
+    }
 }
